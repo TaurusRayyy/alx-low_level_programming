7418fb803af654c6bc9970e557c6852e4157a9df
TaurusRayyy <rchledeh@gmail.com>
2022-10-16 06:46:18 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 include <stdio.h>
 /**
-*main - Entry
-*Return: Always 0
+* main - Entry
+* Return: Always 0
 */
-int main (void)
+int main(void)
 {
-	printf("/"with proper grammar, but the outcome is a piece of art,");
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
