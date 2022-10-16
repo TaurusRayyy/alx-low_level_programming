2810604b21e78882106224253a825602b8a00ca1
TaurusRayyy <rchledeh@gmail.com>
2022-10-16 13:04:42 -0700

    Sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,9 @@ int main(void)
 	float c;
 	double d;
 
-	printf("Size of char = %zu bytes\n", sizeof(a));
-	printf("Size of int = %zu bytes\n", sizeof(b));
-	printf("Size of float = %zu bytes\n", sizeof(c));
-	printf("Size of double = %zu byte\n", sizeof(d));
+	printf("Size of char = %d bytes\n", sizeof(a));
+	printf("Size of int = %d bytes\n", sizeof(b));
+	printf("Size of float = %d bytes\n", sizeof(c));
+	printf("Size of double = %d byte\n", sizeof(d));
 	return (0);
 }
