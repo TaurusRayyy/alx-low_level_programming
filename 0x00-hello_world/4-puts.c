1e22b07e78c12539d2d6775d2552e3e2b8a5469f
TaurusRayyy <rchledeh@gmail.com>
2022-10-15 19:14:44 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
-/**
-
-   * main - main entry point of the function
-
-    * void - means is not taking any value
-
-     * main - Prints Programming is like building a multilingual puzzle
-
-      * return - Alway return the value 0 when successful
-
-       */
-
 int main(void)
 
 {
 
-	        putchar("\"Programming is like building a multilingual puzzle");
-		        return (0);
+	  puts("\"Programming is like building a multilingual puzzle");
+
+	    return (0);
+
 }
