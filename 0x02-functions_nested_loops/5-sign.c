1664392f2a2483558242727bb4d53667a8439d13
TaurusRayyy <rchledeh@gmail.com>
2022-10-20 17:49:00 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
 * @n: The number of which the sign will be printed.
 *
 * Return: 1 if the number is greater than zero,
-* 	0 if the number is zero,
+*	0 if the number is zero,
 *	-1 if the number is less than zero.
 */
 int print_sign(int n)
