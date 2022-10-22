7393328dd65523e1c06daa0c841b074d4f9c9655
TaurusRayyy <rchledeh@gmail.com>
2022-10-21 17:03:54 -0700

    Natural numbers

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -3,28 +3,28 @@
 
    * main - check the code.
 
-    * print_times_table - Prints the n times table.
+    *
 
      * Return: Always 0.
 
       */
 
-int main(void)
-
+void times_table(void)
 {
-	    print_times_table(3);
+
+	    times_table(3);
 
 	        _putchar('\n');
 
-		    print_times_table(5);
+		    times_table(5);
 
 		        _putchar('\n');
 
-			    print_times_table(98);
+			   times_table(98);
 
 			        _putchar('\n');
 
-				    print_times_table(12);  
+				   times_table(12);
 
 				        return (0);
 
