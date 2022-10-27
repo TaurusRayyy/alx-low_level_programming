7da797076d3c86b67f131ebade9192f506b56a9a
TaurusRayyy <rchledeh@gmail.com>
2022-10-26 20:08:32 -0700

    Prime factors

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,16 +7,19 @@
 * Return: 0
 */
 void print_triangle(int size)
+int _putchar(char c)
 {
 	int a, b;
 
 	if (size <= 0)
 		_putchar('\n');
-	for (a = 1; a <= size; a++)
+	{
+		for (a = 1; a <= size; a++)
+		}
 	{
 		for (b = 1; b <= size; b++)
-		{			
-			if (b <= (size -a))
+		{
+			if (b <= (size - a))
 				_putchar(' ');
 			else
 				_putchar('#');
