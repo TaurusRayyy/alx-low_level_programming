d4a234c9e95ad2589bd0efca070adf9a7585d5c3
TaurusRayyy <rchledeh@gmail.com>
2022-10-26 19:17:22 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 /**
 * main - entry point
 * Description: prints the numbers 1 - 100
-* Fizz for multiples of 3, Buzz for 5, 
+* Fizz for multiples of 3, Buzz for 5,
 * and FizzBuzz for multiples of both
 * Return: 0
 */
