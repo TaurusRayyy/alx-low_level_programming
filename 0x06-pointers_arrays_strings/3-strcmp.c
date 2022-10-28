2c6b8114bd73457d937ba7eb05ec1ae885a3dda1
TaurusRayyy <rchledeh@gmail.com>
2022-10-27 18:19:11 -0700

    Compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
 * _strcmp - compare two strings
 * @s1: A pointer to the first string to be compared
 * @s2: A pointer to the second string to be compared
-* 
+*
 * Return: 0
 */
 int _strcmp(char *s1, char *s2)
