0bfef858a63e377b367a50247015dabd608e6999
TaurusRayyy <rchledeh@gmail.com>
2022-10-27 18:47:19 -0700

    changes to lowercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* string_toupper - changes all lowercase letters
+*
+* @str: the string to be changed
+*
+*
+* Return: changed string
+*/
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
